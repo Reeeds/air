f171fe5ec8f71290d1cd0bf46e5e4bc855de57f1
reeeds <reto.schuermann@gmail.com>
2021-09-20 23:12:04 +0200

    xcoms löschen

--- pre.py
+++ pre.py
@@ -42,7 +42,7 @@ def cleanup_xcom(context, session=None):
     dag_id = context["ti"]["dag_id"]
     session.query(XCom).filter(XCom.dag_id == dag_id).delete()
 
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['pre'],on_success_callback=cleanup_xcom())
+@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['pre'],on_success_callback=cleanup_xcom)
 def pre():
 
     @task()
